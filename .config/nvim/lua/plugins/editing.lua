90b3656128d70b5ffbaa74ea97ac3ae0654d721b
Daniel Puterman <1432769+danielputerman@users.noreply.github.com>
2025-12-01 15:01:56 +0200

    added Conform formatter, updated some plugins

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -2,4 +2,32 @@ return {
   -- Surround and commentary
   { "tpope/vim-surround" },
   { "tpope/vim-commentary" },
+
+  -- Formatting
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>j',
+        function()
+          require('conform').format({ async = true, lsp_fallback = true })
+        end,
+        mode = '',
+        desc = 'Format buffer',
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        json = { 'prettier' },
+        jsonc = { 'prettier' },
+	 python = { "isort", "black" },
+	lua = { 'stylua' },
+	markdown = { 'prettier' },
+	sh = { 'shfmt' },
+	yaml = { 'prettier' },
+      },
+    },
+  },
 }
