008564fbe5b9d2255549eb10cb615f26bf7ca688
Daniel Puterman <1432769+danielputerman@users.noreply.github.com>
2026-04-02 23:33:48 +0300

    nvim: fixed treesitter error, still have 'ruff' related problem when python filetypes are loaded

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,49 +1,33 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function() 
-      local configs = require("nvim-treesitter.config")
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter").install({
+      "bash",
+      "c",
+      "dockerfile",
+      "go",
+      "html",
+      "java",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "python",
+      "rust",
+      "typescript",
+      "vim",
+      "yaml",
+    })
 
-      configs.setup({
-	      ensure_installed = {
-		"bash",
-		"c",
-		"dockerfile",
-		"go",
-		"html",
-		"java",
-		"javascript",
-		"json",
-		"lua",
-		"markdown",
-		"python",
-		"rust",
-		"typescript",
-		"vim",
-		"yaml",
-	      },
-          -- A list of parser names, or "all" (the five listed parsers should always be installed)
-          -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html" },
-
-          -- Install parsers synchronously (only applied to `ensure_installed`)
-          sync_install = false,
-
-          -- Automatically install missing parsers when entering buffer
-          -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-          auto_install = true,
-
-          highlight = {
-            enable = true,
-
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
-          },
-          
-          indent = { enable = true },  
-      })
-    end
+    -- Enable treesitter highlighting for common filetypes
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "bash", "c", "dockerfile", "go", "html", "java", "javascript",
+        "json", "lua", "markdown", "python", "rust", "typescript", "vim", "yaml",
+      },
+      callback = function() vim.treesitter.start() end,
+    })
+  end,
 }
