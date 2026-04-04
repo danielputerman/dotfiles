d4841715b5493f21a94c7e39d681d4d5dbeed826
Daniel Puterman <1432769+danielputerman@users.noreply.github.com>
2026-04-04 22:38:45 +0300

    nvim: removed unused python formatters (black/isort) + fixing lualine and catpuccin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -2,10 +2,10 @@ return {
   -- Powerline-like status line
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'catppuccin/nvim' },
     config = function()
       require('lualine').setup({
-        options = { theme = 'catppuccin' }
+        options = { theme = 'catppuccin-mocha' }
       })
     end,
   },
