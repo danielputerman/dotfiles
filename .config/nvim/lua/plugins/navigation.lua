0ccd5116053f158fd3c8a7b971c272acc67ded14
Daniel Puterman <1432769+danielputerman@users.noreply.github.com>
2026-04-05 16:21:26 +0300

    nvim: nvim-tree automatically opens when 'nvim .' is called, showing the files in the current folder (ide like)

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -36,6 +36,16 @@ return {
 
       -- Keybinding
       vim.keymap.set('n', '<leader>n', '<cmd>NvimTreeToggle<cr>', { desc = 'Toggle file explorer' })
+
+      -- Auto-open nvim-tree when launched with a directory
+      vim.api.nvim_create_autocmd('VimEnter', {
+        callback = function(data)
+          if vim.fn.isdirectory(data.file) == 1 then
+            vim.cmd.cd(data.file)
+            require('nvim-tree.api').tree.open()
+          end
+        end,
+      })
     end,
   },
 }
